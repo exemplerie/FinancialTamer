38b7e79138a1b6057148dbff8542b2aa58f432f5
exemplerie <exemplerie@mail.ru>
2025-06-14 03:33:03 +0300

    Initial Commit

---
+++ FinancialTamer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinancialTamer
+//
+//  Created by Валерия Харина on 14.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinancialTamer/FinancialTamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialTamerApp.swift
+//  FinancialTamer
+//
+//  Created by Валерия Харина on 14.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct FinancialTamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
